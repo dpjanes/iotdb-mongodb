4a246a7f686cffed6bdd45215a176a0c3e3063ae
David Janes <davidjanes@davidjanes.com>
2017-08-06 04:43:25 -0400

    works

--- dynamodb/put.js
+++ dynamodb/put.js
@@ -38,9 +38,32 @@ const put = (_self, done) => {
     const self = _.d.clone.shallow(_self)
     const method = "put";
 
+    assert.ok(self.mongodbd, `${method}: expected self.mongodbd`)
+    assert.ok(self.mongodbd.schema, `${method}: expected self.mongodbd.schema`)
     assert.ok(self.mongo_db, `${method}: expected self.mongo_db`)
     assert.ok(self.table_name, `${method}: expected self.table_name`)
     assert.ok(_.is.JSON(self.json), `${method}: expected self.json to be a JSON-like object`)
+
+    const table_schema = self.mongodbd.schema[self.table_name]
+    assert.ok(table_schema, `${method}: expected table_schema for ${self.table_name}`)
+    assert.ok(table_schema.keys, `${method}: expected table_schema.keys for ${self.table_name}`)
+
+    const values = table_schema.keys.map(key => self.json[key] || null)
+    const query = _.object(table_schema.keys, values)
+    const sort = table_schema.keys.map(key => [ key, 1 ])
+
+    Q(self)
+        .then(mongo.collection)
+        .then(sd => {
+            sd.mongo_collection.findAndModify(query, sort, self.json, { w: 1, upsert: true, }, (error, doc) => {
+                if (error) {
+                    return done(error);
+                }
+
+                done(null, self);
+            })
+        })
+        .catch(done)
 }
 
 /**
