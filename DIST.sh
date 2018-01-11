42bb97b5d69a3f2540f91af0978857e4e6db5968
David Janes <david@consensas.com>
2018-01-11 06:02:50 -0500

    add a logger function

--- DIST.sh
+++ DIST.sh
@@ -36,6 +36,7 @@ echo "=================="
         README.md LICENSE \
         package.json \
         index.js \
+        logger.js \
         lib/*.js \
         dynamodb/*.js \
         bin/*.js \
