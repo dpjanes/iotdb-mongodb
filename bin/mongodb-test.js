7ec58db69384ada6a1c21fe4b8ea858214d019e5
David Janes <david@consensas.com>
2018-05-28 15:03:57 -0400

    test a mongodb URL

---
+++ bin/mongodb-test.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+/**
+ *  mongodb-test.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2018-05-28
+ *
+ *  Copyright (2013-2018) David Janes
+ */
+
+"use strict";
+
+const _ = require("iotdb-helpers");
+const fs = require("iotdb-fs");
+
+const path = require("path");
+const os = require("os");
+
+const minimist = require('minimist');
+
+const mongo = require("../index");
+
+const ad = minimist(process.argv.slice(2), {
+    binary: [],
+});
+
+if (ad._.length < 1) {
+    console.log("usage: mongodb-test <url>")
+    process.exit(1)
+}
+
+/*
+ *  Load settings
+ */
+_.promise.make({
+    mongodbd: {
+        url: ad._[0]
+    }
+})
+    .then(mongo.initialize)
+    .then(sd => {
+        console.log("+", "ok")
+    })
+    .catch(error => {
+        console.log("ERROR", error)
+        console.log("#", _.error.message(error))
+    })
+    .done(() => process.exit())
