56d19a80bf40f9f0fe9102444c1a00a75bd33a12
David Janes <david@consensas.com>
2019-02-08 10:54:29 -0500

    .gitignore + list functions

---
+++ universal/list_query.js
@@ -0,0 +1,88 @@
+/*
+ *  universal/list_query.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2019-01-08
+ *
+ *  Copyright [2013-2019] David P. Janes
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict"
+
+const _ = require("iotdb-helpers")
+const mongodb = require("..")
+
+const assert = require("assert")
+
+/**
+ */
+const list_query = _util => {
+    assert(_.is.String(_util.name))
+    assert(_.is.String(_util.one))
+    assert(_.is.String(_util.many))
+    assert(_.is.Function(_util.scrub))
+    assert(_.is.Function(_util.setup))
+    assert(_.is.Function(_util.validate))
+
+    const f = _.promise((self, done) => {
+        _.promise(self)
+            .validate(f)
+
+            .then(_util.setup)
+            .add("query", self.query)
+
+            .then(mongodb.db.list)
+            .each({
+                method: _util.scrub,
+                inputs: `jsons:${_util.one}`,
+                outputs: _util.many,
+                output_selector: sd => sd[_util.one],
+                output_filter: x => x,
+            })
+
+            .end(done, self, _util.many, "cursor")
+    })
+
+    f.method = `${_util.name}.list_query`
+    f.description = `Return records ${_util.one} matching query`
+    f.requires = {
+        query: _.is.Dictionary,
+    }
+    f.accepts = {
+        pager: [ _.is.Integer, _.is.String ],
+        limit: _.is.Integer, 
+    }
+    f.produces = {
+        [ _util.many ]: _.is.Array,
+    }
+
+    /**
+     *  Parameterized
+     */
+    f.p = query => _.promise((self, done) => {
+        _.promise(self)
+            .add("query", query)
+            .then(f)
+            .end(done, self, _util.one, _util.primary_key)
+    })
+
+    return f
+}
+
+/**
+ *  API
+ */
+exports.list_query = list_query
---
+++ universal/list_value.js
@@ -0,0 +1,97 @@
+/*
+ *  universal/list_value.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2019-01-07
+ *
+ *  Copyright [2013-2019] David P. Janes
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict"
+
+const _ = require("iotdb-helpers")
+const mongodb = require("..")
+
+const assert = require("assert")
+
+/**
+ */
+const list_value = (_util, _key, _index) => {
+    assert(_.is.String(_util.name))
+    assert(_.is.String(_util.one))
+    assert(_.is.String(_util.many))
+    assert(_.is.Function(_util.scrub))
+    assert(_.is.Function(_util.setup))
+    assert(_.is.Function(_util.validate))
+    assert(_.is.String(_key))
+    assert(_.is.String(_index) || !_index)
+
+    const f = _.promise((self, done) => {
+        _.promise(self)
+            .validate(f)
+
+            .then(_util.setup)
+            .conditional(self.mongodb$index, _.promise.add("index_name", self.mongodb$index))
+            .conditional(self.mongodb$limit, _.promise.add("query_limit", self.mongodb$limit))
+            .conditional(self.mongodb$start, _.promise.add("pager", self.mongodb$start))
+            .make(sd => {
+                sd.query = {
+                    [ _key ]: sd[_key],
+                }
+            })
+
+            .then(mongodb.db.list)
+            .each({
+                method: _util.scrub,
+                inputs: `jsons:${_util.one}`,
+                outputs: _util.many,
+                output_selector: sd => sd[_util.one],
+                output_filter: x => x,
+            })
+
+            .end(done, self, _util.many, "cursor")
+    })
+
+    f.method = `${_util.name}.list_value`
+    f.description = `Return records ${_util.one} matching ${_key}`
+    f.requires = {
+    }
+    f.accepts = {
+        pager: [ _.is.Integer, _.is.String ],
+        limit: _.is.Integer, 
+    }
+    f.produces = {
+        [ _util.many ]: _.is.Array,
+    }
+
+    /**
+     *  Parameterized
+     */
+    f.p = value => _.promise((self, done) => {
+        _.promise(self)
+            .add(_key, value)
+            .then(f)
+            .end(done, self, _util.one, _util.primary_key)
+    })
+
+    return f
+}
+
+/**
+ *  API
+ */
+exports.list_value = list_value
+
