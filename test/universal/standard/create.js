db4d8517b81a6429e6c5072734a81e40d2d5679e
David Janes <david@consensas.com>
2020-01-20 06:59:54 -0330

    update copyright

--- test/universal/standard/create.js
+++ test/universal/standard/create.js
@@ -5,7 +5,7 @@
  *  IOTDB
  *  2019-02-08
  *
- *  Copyright [2013-2019] David P. Janes
+ *  Copyright (2013-2020) David P. Janes
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
